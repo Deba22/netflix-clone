dca164e995622bd9ab7f2e6608bb9c2d9b721510
Debasish Gracias <d.gracias@clerkswell.com>
2021-02-03 19:55:04 +0000

    first commit

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Banner from './Banner'
+import './HomeScreen.css'
+import Nav from './Nav'
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <Nav/>
+            <Banner/>
+            <h1>test</h1> 
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+
+            <h1>test</h1>
+            <h1>test</h1>
+            <h1>test</h1>
+
+
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAljEL2aYOXP7FsNApWpdqo6Y52a9_ZXQ",
+    authDomain: "netflix-clone-e9ce7.firebaseapp.com",
+    projectId: "netflix-clone-e9ce7",
+    storageBucket: "netflix-clone-e9ce7.appspot.com",
+    messagingSenderId: "896804457421",
+    appId: "1:896804457421:web:89322d6b374e4a31b55284"
+  };
