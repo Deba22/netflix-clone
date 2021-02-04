244117b972b19c831aa18b602cb2b27bc32862d4
Debasish Gracias <d.gracias@clerkswell.com>
2021-02-04 08:04:55 +0000

    minor bugs and fixes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -28,7 +28,7 @@ function Banner() {
         <header className="banner" style={{
             backgroundSize: "cover",
             backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-            backgroundPosition: "center center"
+            backgroundPosition: "center top"
         }}>
             <div className="banner__contents">
                 <h1 className="banner__title">
