139414e09710c0a564476ace17893dfc4ef94da1
Debasish Gracias <d.gracias@clerkswell.com>
2021-02-05 13:36:50 +0000

    authentication added

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAAljEL2aYOXP7FsNApWpdqo6Y52a9_ZXQ",
     authDomain: "netflix-clone-e9ce7.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "896804457421",
     appId: "1:896804457421:web:89322d6b374e4a31b55284"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
